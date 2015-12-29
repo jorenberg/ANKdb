c3fded3219e62dd3afabed74aa76f541b2e40431
Sequømics <sequomics@users.noreply.github.com>
2015-12-29 11:28:25 +0530

    Initial commit
    metadata of ANKdb®.

--- pub/data/reporter.py
+++ pub/data/reporter.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+
+__company__ 	= 'Sequømics Corporation'
+__homepage__ 	= 'http://sequomics.com/'
+__account__ 	= 'SequomicsCorporation'
+__githubURL__ 	= 'https://github.com/SequomicsCorporation'
+__authors__ 	= [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Aiswarya Thomas" <aiswarya.sequomics@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>'
+    ]
