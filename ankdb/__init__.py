5d2f8a77a4660355a55329a500a1b1bfe458db73
Sequømics <sequomics@users.noreply.github.com>
2015-12-31 21:00:36 +0530

    Initial commit
    metadata of ANKdb®.

--- ankdb/__init__.py
+++ ankdb/__init__.py
@@ -1,3 +1,5 @@
 """Ankyrins proteins database by Sequømics Corporation."""
 __author__  = 'Prabhat Kumar'
 __license__ = 'Apache License'
+__version__ = '1.0.0'
+__release__ = __version__ # + '-dev' # comment out '-dev' before a release
